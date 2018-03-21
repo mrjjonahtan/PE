74b8d0063ed4e6d5486ce73932edbb353d1fae50
jonathan195628 <jonathan195628@outlook.com>
2018-03-21 19:12:09 +0800

    texts ing

--- PEParsing/PEEditMessage.cpp
+++ PEParsing/PEEditMessage.cpp
@@ -4,6 +4,8 @@
 
 PEEditMessage::PEEditMessage()
 {
+	message = (wchar_t*)malloc(sizeof(wchar_t) * 0x7530);
+	temChar = (wchar_t*)malloc(sizeof(wchar_t) * 0x400);
 }
 
 
@@ -12,9 +14,34 @@ PEEditMessage::~PEEditMessage()
 	if (edtHwnd != NULL) {
 		edtHwnd = NULL;
 	}
+	if (message != NULL) {
+		free(message);
+		message = NULL;
+	}
+	if (temChar != NULL) {
+		free(temChar);
+		temChar = NULL;
+	}
 }
 
 void PEEditMessage::setMessageText(HWND hDlg, BYTE *pointer) {
+	if (message == NULL || temChar == NULL) {
+		return;
+	}
+	memset(message, 0, sizeof(wchar_t) * 0x7530);
+	memset(temChar, 0, sizeof(wchar_t) * 0x400);
+
+	
+	additionalCharts(L"================================================================================================\r\n");
 	edtHwnd = GetDlgItem(hDlg, IDC_EDIT_MESSAGE);
-	OutputDebugString(L"sss");
+	SendMessage(edtHwnd, WM_SETTEXT, NULL, (WPARAM)message);
+} 
+
+void PEEditMessage::additionalCharts(wchar_t *messageT) {
+	if (message == NULL || temChar == NULL) {
+		return;
+	}
+	wsprintf(temChar, L"%s", messageT);
+	wcsncat_s(message, sizeof(wchar_t) * 0x400, temChar, _TRUNCATE);
+	memset(temChar, 0, sizeof(wchar_t) * 0x400);
 }
--- PEParsing/PEEditMessage.h
+++ PEParsing/PEEditMessage.h
@@ -3,10 +3,14 @@ class PEEditMessage
 {
 private:
 	HWND edtHwnd = NULL;
+	wchar_t *message = NULL;
+	wchar_t *temChar = NULL;
 public:
 	PEEditMessage();
 	~PEEditMessage();
 public:
 	void setMessageText(HWND hDlg, BYTE *pointer);
+private:
+	void additionalCharts(wchar_t *messageT);
 };
 
